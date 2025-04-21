d48d45a01ae606e7ee680612606be79ae25faccd
Dipesh <gautamdipes.com>
2025-04-21 07:56:51 +0545

    master

---
+++ app/src/main/java/com/example/firstproject/DAY 1.kt
@@ -0,0 +1,38 @@
+@file:Suppress("NAME_SHADOWING")
+
+package com.example.reallygreat
+
+fun main() {
+//    //immutable
+//    val age =30
+//    age =20
+//
+//    //mutable
+//    var name = "Prashant"
+//    name= "Really Great"
+//
+//    //in java
+//    String name = "Prashant"
+//    var name : String
+//    name = "Prashant"
+//    println("Hello I am ${name.lowercase() and I am $age year old ")
+//        var age = arrayOf(10,20,30)
+//        age[3] =50 
+//        print (age[3])
+
+//    println("Hello World")
+
+    var male = arrayListOf<String> ("ram", "shyam")
+    var female = arrayListOf<Any>()
+
+    female.add ("sita")
+    female.add ("1")
+    female.add ("Gita")
+    male.removeAt(index:0)
+
+    var  meaning == mapOf(
+        "samsung"to "This is mobile device",
+        "s" to "This is country"
+    )
+
+}
---
+++ app/src/main/java/com/example/firstproject/lesson 1.kt
@@ -0,0 +1,7 @@
+package com.example.firstproject
+
+
+fun main(args: Array<String>) {
+    print("Hello world");
+
+}
